3a6e3312746829bb08264940cc2d60276d9c253f
flpautot <flpautot@airfrance.fr>
2019-08-16 03:24:57 +0200

    add script to setup AWS EC2

---
+++ setupAWS.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo yum update -y
+sudo yum install make glibc-devel gcc patch -y
+sudo yum install gcc-c++ -y
+sudo yum install git -y 
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+nvm install node
