0fa834c15867ed9d7f4242a81d755de033b84480
flpautot <flpautot@airfrance.fr>
2019-08-16 03:56:29 +0200

    Add readme, start script for npm, and AWS setup script

--- setupAWS.sh
+++ setupAWS.sh
@@ -4,4 +4,8 @@ sudo yum install make glibc-devel gcc patch -y
 sudo yum install gcc-c++ -y
 sudo yum install git -y 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+. .bashrc
 nvm install node
+git clone https://github.com/florianpautot/crypto-trading-bot.git bot
+cd bot
+npm install
