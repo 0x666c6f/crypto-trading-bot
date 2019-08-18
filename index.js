eb82f38053af838d8363439b6cf45e22ebb83950
flpautot <flpautot@airfrance.fr>
2019-08-18 22:34:12 +0200

    Add starting log

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var tradeio = require('./tradeio');
 var arbitrageBot = require('./arbitrage-bot')
 var trading_utils = require('./trading-utils')
+var log = require("./logger").logger
+
+log.green("/////////////////////////////////////////////")
+log.green("///// Starting Trade IO Arbitrageg BOT /////")
+log.green("/////////////////////////////////////////////")
+
 
 require('./config.js')
 
