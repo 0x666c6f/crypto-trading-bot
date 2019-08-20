8b80fe8f6f547bf1661a2a8a927f63ce7218fc66
flpautot <flpautot@airfrance.fr>
2019-08-20 14:09:04 +0200

    improve logging timestamping

--- logger.js
+++ logger.js
@@ -1,41 +1,36 @@
-const ansi = require('ansicolor').nice
-const fs = require('fs')
+const ansi = require('ansicolor').nice;
+const fs = require('fs');
 
 const infoLogFile = 'logs/info.log';
-const tradeLogFile = 'logs/trade.log'
-const errorLogFile = 'logs/error.log'
-const logFolder = 'logs'
+const errorLogFile = 'logs/error.log';
+const logFolder = 'logs';
 
-createLogFolder(logFolder)
-createFile(infoLogFile)
-createFile(tradeLogFile)
-createFile(errorLogFile)
+createLogFolder(logFolder);
+createFile(infoLogFile);
+createFile(errorLogFile);
 
 const logger = require('ololog').configure({
-    time: true,
+    time: { yes: true, format: 'iso' } ,
     'render+'(text, {
         consoleMethod = ''
     }) {
         if (text) {
-            const strippedText = ansi.strip(text).trim() + '\n' // remove ANSI codes
-            fs.appendFileSync('logs/info.log', strippedText)
+            const strippedText = ansi.strip(text).trim() + '\n';
+            fs.appendFileSync('logs/info.log', strippedText);
 
-            if ((consoleMethod === 'green')) {
-                fs.appendFileSync('logs/trade.log', strippedText)
-            }
             if ((consoleMethod === 'error') || (consoleMethod === 'warn')) {
 
-                fs.appendFileSync('logs/error.log', strippedText)
+                fs.appendFileSync('logs/error.log', strippedText);
             }
         }
-        return text
+        return text;
     }
-})
+});
 
 
 function createFile(filename) {
     try {
-        fs.openSync(filename, 'r')
+        fs.openSync(filename, 'r');
     } catch (error) {
         fs.writeFileSync(filename, '', function (err) {
         });
@@ -44,13 +39,13 @@ function createFile(filename) {
 
 function createLogFolder(folderPath) {
     try {
-        fs.readdirSync(folderPath)
+        fs.readdirSync(folderPath);
     } catch (error) {
         try {
-            fs.mkdirSync(folderPath, "777")
+            fs.mkdirSync(folderPath, "777");
             return;
         } catch (error) {
         }
     }
 }
-exports.logger = logger
+exports.logger = logger;
