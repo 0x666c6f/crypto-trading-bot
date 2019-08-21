af87e20f01ae22b4bc3bac1d283ea744288c0268
flpautot <flpautot@airfrance.fr>
2019-08-21 14:50:48 +0200

    update aws set up script

--- setupAWS.sh
+++ setupAWS.sh
@@ -3,10 +3,11 @@ sudo yum update -y
 sudo yum install make glibc-devel gcc patch -y
 sudo yum install gcc-c++ -y
 sudo yum install git -y 
-git config credential.helper store
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
 . .bashrc
 nvm install node
+npm install pm2 -g
 git clone https://github.com/florianpautot/crypto-trading-bot.git bot
 cd bot
+git config credential.helper store
 npm install
