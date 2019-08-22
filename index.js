b495cb6bb41a157fca5d9cb5d748de1c3e907616
flpautot <flpautot@airfrance.fr>
2019-08-22 09:36:53 +0200

    allow bot to start without start date

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var arbitrageBot = require('./arbitrage-bot');
 var trading_utils = require('./trading-utils');
 var log = require("./logger").logger;
 var CronJob = require('cron').CronJob;
+const moment = require("moment");
 
 log.green("/////////////////////////////////////////////");
 log.green("///// Starting Trade IO Arbitrageg BOT /////");
@@ -11,16 +12,34 @@ log.green("/////////////////////////////////////////////");
 
 require('./config.js');
 
-const startDate = new Date(Date.parse(process.env.StartDate));
-log.green("Arbitrage bot starting date is set at :", startDate);
-const job = new CronJob(startDate, function() {
-    log.green("Starting date reached, waking up !");
+try {
+    
+    const startDate = new Date(Date.parse(process.env.StartDate));
+    const startMoment = moment(startDate.getTime());
+    if(startMoment.isAfter(moment.now())){
+        log.green("Arbitrage bot starting date is set at :", startDate);
+        const job = new CronJob(startDate, function() {
+            log.green("Starting date reached, waking up !");
+        
+            tradeio.info().then(function (infos) {
+                infos = trading_utils.formatInfos(infos.symbols);
+                arbitrageBot.start(infos);
+            });
+        });
+        job.start();
+    
+    } else {
+        log.green("No/Wrong start date set Arbitrage bot starting now");
 
-    tradeio.info().then(function (infos) {
-        infos = trading_utils.formatInfos(infos.symbols);
-        arbitrageBot.start(infos);
-    });
-});
-job.start();
+        tradeio.info().then(function (infos) {
+            infos = trading_utils.formatInfos(infos.symbols);
+            arbitrageBot.start(infos);
+        });
+    }
+    
+} catch (error) {
+    log.green("Error while starting bot :",error);
 
+}
+    
 
