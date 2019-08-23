bfedbd432aeb3608bd90c7b179461827fb96e924
flpautot <flpautot@airfrance.fr>
2019-08-23 08:51:45 +0200

    improve start up to keep seconds in starting date

--- index.js
+++ index.js
@@ -12,9 +12,10 @@ log.green("/////////////////////////////////////////////");
 
 require('./config.js');
 
+const startDate = new Date(Date.parse(process.env.StartDate));
+
 try {
     
-    const startDate = new Date(Date.parse(process.env.StartDate));
     const startMoment = moment(startDate.getTime());
     if(startMoment.isAfter(moment.now())){
         log.green("Arbitrage bot starting date is set at :", startDate);
@@ -29,12 +30,22 @@ try {
         job.start();
     
     } else {
-        log.green("No/Wrong start date set Arbitrage bot starting now");
+        log.green("No/Wrong start date set Arbitrage bot will only keep seconds in date");
 
-        tradeio.info().then(function (infos) {
-            infos = trading_utils.formatInfos(infos.symbols);
-            arbitrageBot.start(infos);
+        let start = moment().add(1,"minute");
+        start.set("second", startDate.getSeconds());
+        
+        log.green("Arbitrage bot starting date is set at :", start.toDate());
+
+        const job = new CronJob(start, function() {
+            log.green("Starting date reached, waking up !");
+        
+            tradeio.info().then(function (infos) {
+                infos = trading_utils.formatInfos(infos.symbols);
+                arbitrageBot.start(infos);
+            });
         });
+        job.start();
     }
     
 } catch (error) {
