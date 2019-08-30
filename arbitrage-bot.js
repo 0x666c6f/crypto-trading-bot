71811bec24d109b0aeb1f8881ce278c5438be4a7
flpautot <flpautot@airfrance.fr>
2019-08-31 00:05:41 +0200

    fix again sleep management

--- arbitrage-bot.js
+++ arbitrage-bot.js
@@ -33,7 +33,7 @@ var start = async function (infos) {
         while (totalHourlyWeight < (process.env.APIHourlyLimit - 23) && (totalHourlyOrderWeight < process.env.OrderHourlyLimit - 3) && moment().isBefore(endHourDate)) {
             var endMinuteDate = moment().add(1, "minute").set("second", process.env.EndSecond).milliseconds(0);
             var endSecond = moment().set("second", process.env.EndSecond).milliseconds(0);
-            var restartDate = moment().set("second", process.env.StartSecond).milliseconds(0);
+            var restartDate = moment().add(1, "minute").set("second", process.env.StartSecond).milliseconds(0);
             // log("Hourly Weights :")
             // log("Hourly Weight :", totalHourlyWeight)
             // log("Hourly Order Weight :", totalHourlyOrderWeight)
