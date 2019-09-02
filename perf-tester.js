e198fc6c2ad53a96febaec63459a5a2807992d2f
flpautot <flpautot@airfrance.fr>
2019-09-02 21:23:51 +0200

    add perf tester to check actual response time

---
+++ perf-tester.js
@@ -0,0 +1,26 @@
+var log = require("./logger").logger;
+var http = require('./http-manager');
+
+require('./config.js');
+
+var tickers = function (symbol) {
+    log("TradeIO Tickers Request")
+    return new Promise((resolve, reject) => {
+        http.get(process.env.APIEndpoint + "/api/v1/tickers", false, null).then(function (resp) {
+            log("Tickers successfull")
+            resolve(resp);
+        }, function (error) {
+            log.red("Error while doing Tickers request = "+ JSON.stringify(error, null, 2))
+            reject(error);
+        }).catch(function (err) {
+            log.red("Error while doing Tickers request = "+ JSON.stringify(err, null, 2))
+            reject(err);
+        });
+    });
+};
+
+var start = new Date().getTime();
+tickers().then( () => {
+    log("Duration = ", new Date().getTime() - start);
+});
+
