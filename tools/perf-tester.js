ea3c53969e75fd4da0710606cb572ca0f0057074
flpautot <flpautot@airfrance.fr>
2019-09-03 17:01:30 +0200

    improve perf tester

--- tools/perf-tester.js
+++ tools/perf-tester.js
@@ -3,7 +3,9 @@ var http = require('../lib/http-manager');
 
 require('../config/config.js');
 
-var tickers = function (symbol) {
+const requestNb = process.argv[2];
+
+var tickers = async function (symbol) {
     log("TradeIO Tickers Request")
     return new Promise((resolve, reject) => {
         http.get(process.env.APIEndpoint + "/api/v1/tickers", false, null).then(function (resp) {
@@ -19,8 +21,21 @@ var tickers = function (symbol) {
     });
 };
 
-var start = new Date().getTime();
-tickers().then( () => {
-    log("Duration = ", new Date().getTime() - start);
-});
+var processAverageDuration = async function (){
+    let total = 0;
+    for (let index = 0; index < requestNb; index ++)
+    {
+        var start = new Date().getTime();
+        await tickers();
+        total += new Date().getTime() - start;
+        log("Duration = ", new Date().getTime() - start);
+        
+    }
+    log.green("Average request time = ", total/requestNb, "ms");
+
+}
+
+processAverageDuration();
+
+
 
